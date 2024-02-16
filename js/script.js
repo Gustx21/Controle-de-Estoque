6ab58dc7fea51ea6c4e955e50b6b04852ef5a180
Gustx21 <145871288+Gustx21@users.noreply.github.com>
2024-02-16 17:24:14 -0300

    WIN

--- js/script.js
+++ js/script.js
@@ -1,28 +1,22 @@
-let itens = {};
+let itens;
 let estoque = [];
 let opcao = 0;
 
 function criar() {
   let produto, total;
-  let i = 0;
 
-  total = parseInt(prompt("Quantos itens serão adicionados ao estoque:"));
+  produto = prompt("Digite o nome do produto:").toUpperCase().trim();
+  quantidade = parseFloat(prompt(`Digite a quantidade total de ${produto}:`));
 
-  if (isNaN(total) || total <= 0) {
+  if (isNaN(quantidade) || quantidade <= 0) {
     alert("Insira um valor válido.");
     return;
   };
 
-  for (; i < total; i++) {
-    produto = prompt("Digite o nome do produto:").toUpperCase().trim();
-    quantidade = parseFloat(prompt(`Digite a quantidade total de ${produto}:`));
-    
-    itens[produto] = quantidade;
-
-    alert(`O item ${produto} foi adicionado com ${quantidade} unidades.`);
-  };
-
+  itens = { produto, quantidade }
   estoque.push(itens);
+
+  alert(`O item ${produto} foi adicionado com ${quantidade} unidades.`);
 };
 
 function listar() {
@@ -32,7 +26,7 @@ function listar() {
   };
 
   const listarEstoque = estoque.map(function (item, indice) {
-    return `${indice + 1}. ${itens.nome}: ${itens.quantidade} unidades.`
+    return `${indice + 1}. ${itens.produto}: ${itens.quantidade} unidades.`
   }).join("\n");
 
   alert(`Itens no estoque:\n${listarEstoque}`);
@@ -48,17 +42,20 @@ function remover() {
   alert(`O item ${remove} foi removido.`);
 };
 
-while (opcao !== 3) {
-  opcao = parseInt(prompt("Insira uma opção:\n1. Criar.\n2. Remover.\n3. Sair."));
+while (opcao !== 4) {
+  opcao = parseInt(prompt("Insira uma opção:\n1. Criar.\n2. Listar\n3. Remover.\n4. Sair."));
 
   switch (opcao) {
     case 1:
       criar();
       break;
     case 2:
-      remover();
+      listar();
       break;
     case 3:
+      remover();
+      break;
+    case 4:
       alert("Encerrando.");
       break;
     default:
@@ -66,5 +63,3 @@ while (opcao !== 3) {
       break;
   };
 };
-
-console.table(itens);
