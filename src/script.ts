0cc151752dde5c8bdc934ae23228147fac95d12c
Gustavo Andre <gugamathiaz12@gmail.com>
2024-12-01 21:31:41 -0300

    Adicionando filtro

--- src/script.ts
+++ src/script.ts
@@ -1,19 +1,19 @@
 enum Prices {
   M = 1.55,
-  Bauducco = 3.5,
-  Marilan = 2.75,
-  Visconti = 3.0,
-  Linea = 4.5,
-  Nissin = 1.2,
-  Colgate = 2.0,
-  Palmolive = 2.5,
-  Ferrero = 5.0,
-  LaPastina = 6.0,
-  DonaBenta = 3.8,
-  Sazon = 10.8,
-  SCJohnson = 7.5,
-  Cereser = 25.5,
-  Ajinomoto = 4.0
+  BAUDUCCO = 3.5,
+  MARILAN = 2.75,
+  VISCONTI = 3.0,
+  LINEA = 4.5,
+  NISSIN = 1.2,
+  COLGATE = 2.0,
+  PALMOLIVE = 2.5,
+  FERRERO = 5.0,
+  LAPASTINA = 6.0,
+  DONABENTA = 3.8,
+  SAZON = 10.8,
+  SCJOHNSON = 7.5,
+  CERESER = 25.5,
+  AJINOMOTO = 4.0
 }
 
 interface InventoryItems {
@@ -30,15 +30,11 @@ async function criar(event: Event): Promise<void> {
   event.preventDefault();
 
   try {
-    const productInput = document.getElementById("product") as HTMLInputElement;
-    const quantityInput = document.getElementById("quantity") as HTMLInputElement;
-    const provideInput = document.getElementById("provides") as HTMLInputElement;
+    const product: string = (document.getElementById("product") as HTMLInputElement).value.toUpperCase();
+    const quantity: number = Number((document.getElementById("quantity") as HTMLInputElement).value);
+    const provide: keyof typeof Prices = (document.getElementById("provides") as HTMLInputElement).value.toUpperCase() as keyof typeof Prices;
 
-    const product: string = productInput.value.toUpperCase();
-    const quantity: number = Number(quantityInput.value.toUpperCase());
-    let provide: keyof typeof Prices = provideInput.value.toUpperCase() as keyof typeof Prices;
-
-    if (/[\d\s]/.test(product) || quantity <= 0 || quantity > 10000) {
+    if (/[\d]/.test(product) || quantity <= 0 || quantity > 10000) {
       throw new Error("Dados inválidos!");
     };
 
@@ -57,48 +53,78 @@ async function criar(event: Event): Promise<void> {
       body: JSON.stringify(stock)
     });
 
-    setTimeout(() => { listar() }, 500);
+    setTimeout(() => { window.location.reload() }, 500);
   } catch (error) {
     console.error(`Error de dados: ${error}`);
   }
 };
 
-document.addEventListener("DOMContentLoaded", listar);
+document.addEventListener("DOMContentLoaded", consultar);
 
-async function listar(): Promise<any> {
+async function consultar(): Promise<void> {
   try {
     const response: Response = await fetch("http://localhost:3000/inventory");
     const stock: InventoryItems[] = await response.json();
 
-    // Seção da Tabela
-    const sectionTable = document.getElementById("stock") as HTMLDataElement;
-    
-    // Corpo da tabela
-    const tbody = document.createElement('tbody');
-    const row = document.createElement('tr');
-    
-    stock.forEach((item: InventoryItems) => {
-      const productCell = document.createElement('td');
-      productCell.textContent = item.product;
-
-      const quantityCell = document.createElement('td');
-      quantityCell.textContent = item.quantity.toString();
-
-      const priceCell = document.createElement('td');
-      priceCell.textContent = item.price.toString();
-
-      const provideCell = document.createElement('td');
-      provideCell.textContent = item.provide;
-
-      row.append(productCell, quantityCell, priceCell, provideCell)
-      tbody.appendChild(row);
-      sectionTable.append(tbody);
-    });
-
+    listar(stock);
   } catch (error) {
     console.error(`Valores não encontrados: ${error}`);
     setTimeout(() => { window.location.reload() }, 360 * 1000);
   }
+};
+
+document.getElementById("filters")?.addEventListener("click", filtro);
+
+async function filtro(event: Event) {
+  event.preventDefault();
+
+  try {
+    const response: Response = await fetch("http://localhost:3000/inventory");
+    const stock: InventoryItems[] = await response.json();
+
+    const productQuery: string = (document.getElementById("productQuery") as HTMLInputElement)?.value.toLowerCase().trim();
+    const quantityQuery: number = Number((document.getElementById("quantityQuery") as HTMLInputElement)?.value);
+    const priceQuery: number = Number((document.getElementById("priceQuery") as HTMLInputElement)?.value);
+    const provideQuery: string = (document.getElementById("provideQuery") as HTMLInputElement)?.value.toLowerCase().trim();
+
+    if (!productQuery && !quantityQuery && !priceQuery && !provideQuery) {
+      listar(stock);
+      return;
+    };
+
+    const filters = stock.filter((item) => {
+      const matchesProduct = productQuery ? item.product.toLowerCase() === productQuery : true;
+      const matchesQuantity = quantityQuery ? item.quantity === quantityQuery : true;
+      const matchesPrice = priceQuery ? item.price === priceQuery : true;
+      const matchesProvide = provideQuery ? item.provide.toLowerCase() === provideQuery : true;
+
+      return matchesProduct && matchesQuantity && matchesPrice && matchesProvide;
+    });
+
+    listar(filters);
+  } catch (error) {
+    console.error(`Valores não encontrados: ${error}`);
+    setTimeout(() => { window.location.reload() }, 360 * 1000);
+  }
+};
+
+function listar(stock: InventoryItems[]): void {
+  // Seção da Tabela
+  const sectionTable = document.getElementById("stock") as HTMLDataElement;
+  sectionTable.innerHTML = "";
+
+  // Conteúdo da Tabela
+  stock.forEach((item) => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${item.product}</td>
+      <td>${item.quantity}</td>
+      <td>${item.price}</td>
+      <td>${item.provide}</td>
+    `;
+
+    sectionTable.appendChild(row);
+  });
 };
 
 document.getElementById("update")?.addEventListener('click', alterar);
